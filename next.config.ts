888a4dbc0a68d5c0937d673392a7111a25db7490
Your Name <dinukarathnayaka2000@gmail.com>
2025-10-18 00:24:49 +1100

    update next.configue

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         source: "/:path*",
         headers: [
           {
+            key: "X-Frame-Options",
+            value: "ALLOWALL"
+          },
+          {
             key: "Content-Security-Policy",
             value: "frame-ancestors *",
           },
