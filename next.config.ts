0e38bd8a550148af5aead4aeb99107c274b41865
Your Name <dinukarathnayaka2000@gmail.com>
2025-10-18 00:00:25 +1100

    updare next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
