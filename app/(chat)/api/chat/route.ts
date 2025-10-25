bd2297bba236ffe76c979ab8cc9f3828fdf02d90
Your Name <dinukarathnayaka2000@gmail.com>
2025-10-25 13:12:12 +1100

    update backend url

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -100,7 +100,7 @@ export async function POST(request: Request) {
     console.log("user sent new message's Previous Response ID:", previousResponseId);
 
     // call custom backend
-    const backendURL = process.env.BACKEND_URL;
+    const backendURL = process.env.BACKEND_URL || 'http://localhost:8001';
     if (!backendURL) {
       throw new Error("Backend URL is not defined");
     }
